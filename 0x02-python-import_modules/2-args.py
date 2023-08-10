67f1a0aec3caf5bb2c2072641eecee77bfcdf2bc
Everlyne akinyi <evahaudi2354@gmail.com>
2023-08-10 00:44:56 -0700

    updated number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
     userin = argv[1:]
-    size = len(userin)
+    num_args = len(userin)
     print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) != 1 else "argument",
-                 "." if (size) == 0 else ":"))
+          format(num_args,
+                 "arguments" if (num_args) != 1 else "argument",
+                 "." if (num_args) == 0 else ":"))
     for idx, arg in enumerate(userin):
         print("{:d}: {:s}".format(idx + 1, arg))
