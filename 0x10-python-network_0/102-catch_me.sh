48d3d5ffb18cc35401d6832a8c58f508b1e8f494
everlyne akinyi <evahaudi2354@gmail.com>
2023-11-30 10:57:04 -0800

    tayd

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me and get the response
-curl -sX PUT -L -d "user_id=98" -H "Origin:You got me!" "0.0.0.0:5000/catch_me"
+
+curl  -X GET 0.0.0.0:5000/catch_me
